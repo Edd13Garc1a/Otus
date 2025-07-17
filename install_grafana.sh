8ea0cd2bfa816811d29bf0c2557de8a7cae3c830
Edd13Garc1a <143624401+Edd13Garc1a@users.noreply.github.com>
2025-07-17 15:07:59 +0300

    Update install_grafana.sh

--- install_grafana.sh
+++ install_grafana.sh
@@ -2,22 +2,9 @@
 
 echo "Установка Grafana..."
 
-# Директория с пакетами
-PKG_DIR="/home/vboxuser"
-
-# Устанавливаем необходимые пакеты
-sudo apt-get install -y apt-transport-https software-properties-common wget
-
-# Добавляем репозиторий Grafana
-#sudo mkdir -p /etc/apt/keyrings/
-#wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
-#echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
-
-# Обновляем пакеты и устанавливаем Grafana
-sudo apt-get update
-#sudo apt-get install -y grafana
-sudo dpkg -i "${PKG_DIR}/grafana_10.4.1_amd64.deb"
-sudo apt-get install -f -y
+sudo apt-get install -y adduser libfontconfig1 musl
+wget https://dl.grafana.com/oss/release/grafana_10.4.1_amd64.deb
+sudo dpkg -i grafana_10.4.1_amd64.deb
 
 # Запускаем Grafana
 sudo systemctl enable grafana-server
