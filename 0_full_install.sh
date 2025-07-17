1e91b6a0cbc8eb8b9729e55d44bf0dd6df265591
Edd13Garc1a <143624401+Edd13Garc1a@users.noreply.github.com>
2025-07-17 16:19:47 +0300

    Update 0_full_install.sh

--- 0_full_install.sh
+++ 0_full_install.sh
@@ -14,6 +14,9 @@ chmod +x 8_restore_db_master.sh
 echo "Запуск установки nginx, apache2."
 ./2_install.sh
 
+echo "Запуск установки iptables"
+./3_setup_iptables.sh
+
 # раскомментировать при аварином восстановлении
 # echo "Установка свежего бекапа"
 # ./8_restore_db_master.sh
