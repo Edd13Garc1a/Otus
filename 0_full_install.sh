69a5ebb19b83a7d8f59d8c42d02ccaec4689eb90
Edd13Garc1a <143624401+Edd13Garc1a@users.noreply.github.com>
2025-07-19 13:45:34 +0300

    Update 0_full_install.sh
    1 2 3

--- 0_full_install.sh
+++ 0_full_install.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 # Делаем скрипты исполняемыми
-chmod +x 2_install.sh
-chmod +x 4_install_mysql.sh
-chmod +x 5_install_monitoring.sh
-chmod +x 6_install_elk_v3.sh
-chmod +x 7_backup_slave_and_push.sh
-chmod +x 8_restore_db_master.sh
+chmod +x 1_install.sh
+chmod +x 2_install_mysql.sh
+chmod +x 3_install_monitoring.sh
+chmod +x 4_install_elk_v3.sh
+chmod +x 5_backup_slave_and_push.sh
+chmod +x 6_restore_db_master.sh
 
 echo "Запуск установки nginx, apache2."
-./2_install.sh
-
-# раскомментировать при аварином восстановлении
-# echo "Установка свежего бекапа"
-# ./8_restore_db_master.sh
+./1_install.sh
 
 echo "Запуск установки и настройки MySQL..."
-./4_install_mysql.sh
+./2_install_mysql.sh
 
 echo "Запуск установки Prometheus, node_exporter, Grafana ..."
-./5_install_monitoring.sh
+./3_install_monitoring.sh
 
 echo "Запуск установки Elasticsearch, Kibana, Filebeat..."
-./6_install_elk_v3.sh
+./4_install_elk.sh
 
 echo "Бекап БД и пуш в репозиторий"
-./7_backup_slave_and_push.sh
-                                               
+./5_backup_slave_and_push.sh
+
+ # раскомментировать при аварином восстановлении
+# echo "Установка свежего бекапа"
+# ./6_restore_db_master.sh
+
 echo "Установка завершена!"
